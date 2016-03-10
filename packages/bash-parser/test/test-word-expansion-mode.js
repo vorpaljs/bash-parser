b488b3f9588fcdedd3937e3c2a032b2ad47cc1ff
Andrea Parodi <andrea@parro.it>
2016-03-11 00:07:11 +0200

    expand-word mode

---
+++ packages/bash-parser/test/test-word-expansion-mode.js
@@ -0,0 +1,31 @@
+'use strict';
+/* eslint-disable camelcase */
+
+const test = require('ava');
+const bashParser = require('../src');
+const utils = require('./_utils');
+
+test('expand on a single word', t => {
+	const result = bashParser('ls $var > res.txt', {
+		mode: 'word-expansion'
+	});
+	// utils.logResults(result);
+	utils.checkResults(t, result, {
+		type: 'Script',
+		commands: [{
+			type: 'Command',
+			name: {
+				type: 'Word',
+				text: 'ls $var > res.txt',
+				expansion: [{
+					parameter: 'var',
+					loc: {
+						start: 3,
+						end: 6
+					},
+					type: 'ParameterExpansion'
+				}]
+			}
+		}]
+	});
+});

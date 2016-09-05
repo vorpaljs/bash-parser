e7f6b74df1c4eedd784dede9db6782c40d7e61d9
Andrea Parodi <andrea@parro.it>
2016-09-05 23:14:44 +0200

    First release

---
+++ packages/iterable-lookahead/index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+function iterableLookahead(iterable, size) {
+	if (size === undefined) {
+		size = 1;
+	}
+
+	if (typeof size !== 'number' && !(size instanceof Number)) {
+		throw new TypeError('Size argument must be a number');
+	}
+
+	if (size < 1) {
+		throw new RangeError('Size argument must be greater than 0');
+	}
+
+	const behindCache = new Array(size + 1);
+	const aheadCache = [];
+
+	const iterator = iterable[Symbol.iterator]();
+
+	return {
+		ahead(idx) {
+			if (idx > size) {
+				throw new RangeError(`Cannot look ahead of ${idx} position, currently depth is ${size}`);
+			}
+
+			if (idx < 1) {
+				throw new RangeError('Look ahead index must be greater than 0');
+			}
+
+			return aheadCache[idx - 1];
+		},
+
+		behind(idx) {
+			if (idx > size) {
+				throw new RangeError(`Cannot look behind of ${idx} position, currently depth is ${size}`);
+			}
+
+			if (idx < 1) {
+				throw new RangeError('Look behind index must be greater than 0');
+			}
+			// console.log({behindCache});
+			return behindCache[idx];
+		},
+
+		[Symbol.iterator]() {
+			return this;
+		},
+
+		next() {
+			let item = iterator.next();
+			while (!item.done && aheadCache.length <= size) {
+				aheadCache.push(item.value);
+				item = iterator.next();
+			}
+			if (!item.done) {
+				aheadCache.push(item.value);
+			}
+
+			if (item.done && aheadCache.length === 0) {
+				return {done: true};
+			}
+
+			const value = aheadCache.shift();
+
+			behindCache.unshift(value);
+			behindCache.pop();
+
+			return {done: false, value};
+		}
+	};
+}
+
+module.exports = iterableLookahead;

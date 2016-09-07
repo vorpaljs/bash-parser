a751fa3cb3838f3ea39c66bd57969884614759d5
Andrea Parodi <andrea@parro.it>
2016-09-07 23:00:54 +0200

    website improvements

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,6 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
-"use strict";require("babel-polyfill");var bashParser=require("../src"),$=global.$,ast=bashParser("echo hello world");$("#ast").html(JSON.stringify(ast,null,2));
+"use strict";function parseSource(){try{$("#error").hide();var r=bashParser($("#source").val());$("#ast").html(JSON.stringify(r,null,2))}catch(e){$("#error").html(e.message),$("#error").show()}}require("babel-polyfill");var bashParser=require("../src"),$=global.$;$("#parse").click(parseSource),parseSource();
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"../src":438,"babel-polyfill":10}],2:[function(require,module,exports){
--- docs/index.js
+++ docs/index.js
@@ -3,5 +3,17 @@ require('babel-polyfill');
 const bashParser = require('../src');
 
 const $ = global.$;
-const ast = bashParser('echo hello world');
-$('#ast').html(JSON.stringify(ast, null, 2));
+
+function parseSource() {
+	try {
+		$('#error').hide();
+		const ast = bashParser($('#source').val());
+		$('#ast').html(JSON.stringify(ast, null, 2));
+	} catch (err) {
+		$('#error').html(err.message);
+		$('#error').show();
+	}
+}
+
+$('#parse').click(parseSource);
+parseSource();

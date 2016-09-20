319cea0890e9bd3c118b7a2122e129390d104bd3
Andrea Parodi <andrea@parro.it>
2016-09-20 20:28:31 +0200

    utils.replaceRule

--- src/modes/bash/index.js
+++ src/modes/bash/index.js
@@ -1,30 +1,22 @@
 'use strict';
 
-const replaceRule = require('../../../packages/array-replace-item');
 const bashAliasSubstitution = require('./rules/alias-substitution');
 
 module.exports = {
 	inherits: 'posix',
-	init: posixMode => {
+	init: (posixMode, utils) => {
 		const phaseCatalog = Object.assign(
 			{},
 			posixMode.phaseCatalog,
 			{bashAliasSubstitution}
 		);
 
-		const lexerPhases = replaceRule(
+		const lexerPhases = utils.replaceRule(
 			phaseCatalog.aliasSubstitution,
 			bashAliasSubstitution,
 			posixMode.lexerPhases
 		);
 
-		return {
-			phaseCatalog,
-			lexerPhases,
-			tokenizer: posixMode.tokenizer,
-			grammarSource: posixMode.grammarSource,
-			grammar: posixMode.grammar,
-			astBuilder: posixMode.astBuilder
-		};
+		return Object.assign({}, posixMode, {phaseCatalog, lexerPhases});
 	}
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,3 @@
 exports.loggerPhase = require('./logger-phase');
 exports.isValidName = require('./is-valid-name');
+exports.replaceRule = require('../../packages/array-replace-item');

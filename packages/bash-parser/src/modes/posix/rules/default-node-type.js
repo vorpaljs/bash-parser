898d1fd23982c145370be23a4ef511267cae7de2
Andrea Parodi <andrea@parro.it>
2016-10-04 20:33:37 +0200

    removed generator from default-node-type transform

--- packages/bash-parser/src/modes/posix/rules/default-node-type.js
+++ packages/bash-parser/src/modes/posix/rules/default-node-type.js
@@ -1,24 +1,24 @@
 'use strict';
 
 const toPascal = require('to-pascal-case');
+const map = require('map-iterable');
 
-module.exports = () => function * defaultNodeType(tokens) {
-	for (const token of tokens) {
-		const tk = Object.assign({}, token);
-		if (tk.type) {
-			tk.originalType = token.type;
-			// console.log({defaultNodeType, tk})
-			if (token.is('WORD') || token.is('NAME') || token.is('ASSIGNMENT_WORD')) {
-				tk.type = toPascal(tk.type);
-			} else {
-				tk.type = token.type.toLowerCase();
-			}
-
-			for (const xp of tk.expansion || []) {
-				xp.type = toPascal(xp.type);
-			}
+module.exports = () => map(token => {
+	const tk = Object.assign({}, token);
+	if (tk.type) {
+		tk.originalType = token.type;
+		// console.log({defaultNodeType, tk})
+		if (token.is('WORD') || token.is('NAME') || token.is('ASSIGNMENT_WORD')) {
+			tk.type = toPascal(tk.type);
+		} else {
+			tk.type = token.type.toLowerCase();
 		}
-		// Object.freeze(tk);
-		yield tk;
+
+		for (const xp of tk.expansion || []) {
+			xp.type = toPascal(xp.type);
+		}
 	}
-};
+	// Object.freeze(tk);
+	return tk;
+});
+

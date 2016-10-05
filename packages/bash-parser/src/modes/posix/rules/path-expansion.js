f605a4cf2572daca2126d1e10354e686a799f50a
Andrea Parodi <andrea@parro.it>
2016-10-05 21:20:28 +0200

    refactored path expansion

--- packages/bash-parser/src/modes/posix/rules/path-expansion.js
+++ packages/bash-parser/src/modes/posix/rules/path-expansion.js
@@ -1,13 +1,17 @@
 'use strict';
-module.exports = (options, utils) => function * pathExpansion(tokens) {
-	for (const token of tokens) {
-		if (token.is('WORD') && typeof options.resolvePath === 'function') {
-			yield utils.tokens.setValue(token, options.resolvePath(token.value));
-		} else if (token.is('ASSIGNMENT_WORD') && typeof options.resolvePath === 'function') {
-			const parts = token.value.split('=');
-			yield utils.tokens.setValue(token, parts[0] + '=' + options.resolvePath(parts[1]));
-		} else {
-			yield token;
-		}
+
+const map = require('map-iterable');
+const tokens = require('../../../utils/tokens');
+
+module.exports = options => map(token => {
+	if (token.is('WORD') && typeof options.resolvePath === 'function') {
+		return tokens.setValue(token, options.resolvePath(token.value));
 	}
-};
+
+	if (token.is('ASSIGNMENT_WORD') && typeof options.resolvePath === 'function') {
+		const parts = token.value.split('=');
+		return tokens.setValue(token, parts[0] + '=' + options.resolvePath(parts[1]));
+	}
+
+	return token;
+});

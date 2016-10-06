26041875ec6d0cb769625c929ce347776452cd09
Andrea Parodi <andrea@parro.it>
2016-10-06 20:51:50 +0200

    refactored simple command names

--- packages/bash-parser/src/modes/posix/rules/identify-simplecommand-names.js
+++ packages/bash-parser/src/modes/posix/rules/identify-simplecommand-names.js
@@ -1,40 +1,55 @@
 'use strict';
+const lookahead = require('iterable-lookahead');
+const compose = require('compose-function');
+const map = require('map-iterable');
 const isOperator = require('../enums/io-file-operators').isOperator;
+const isValidName = require('../../../utils/is-valid-name');
 
-module.exports = (options, utils) => function * identifySimpleCommandNames(tokens) {
-	for (const tk of tokens) {
+function couldEndSimpleCommand(scTk) {
+	return scTk && (
+		scTk.is('SEPARATOR_OP') ||
+		scTk.is('NEWLINE') ||
+		scTk.is('NEWLINE_LIST') ||
+		scTk.value === ';' ||
+		scTk.is('PIPE') ||
+		scTk.is('OR_IF') ||
+		scTk.is('PIPE') ||
+		scTk.is('AND_IF')
+	);
+}
+
+function couldBeCommandName(tk) {
+	return tk && tk.is('WORD') && isValidName(tk.value);
+}
+
+module.exports = () => compose(
+	map((tk, idx, iterable) => {
 		if (tk._.maybeStartOfSimpleCommand) {
-			if (tk.is('WORD') && utils.isValidName(tk.value)) {
+			if (couldBeCommandName(tk)) {
 				tk._.maybeSimpleCommandName = true;
-				yield tk;
-				continue;
-			}
-
-			yield tk;
-
-			let lastToken = tk;
-			let commandNameFound = false;
-			let item = tokens.next();
-			while (!item.done) {
-				const scTk = item.value;
-				if (!commandNameFound && !isOperator(lastToken) && scTk.is('WORD') && utils.isValidName(scTk.value)) {
-					scTk._.maybeSimpleCommandName = true;
-					// scTk.maybeSimpleCommandName = true;
-					commandNameFound = true;
+			} else {
+				const next = iterable.ahead(1);
+				if (next && !couldEndSimpleCommand(next)) {
+					next._.commandNameNotFoundYet = true;
 				}
-
-				yield scTk;
-
-				if (scTk.is('SEPARATOR_OP') || scTk.is('NEWLINE') || scTk.is('NEWLINE_LIST') || scTk.value === ';' ||
-					scTk.is('PIPE') || scTk.is('OR_IF') || scTk.is('PIPE') || scTk.is('AND_IF')) {
-					break;
-				}
-
-				lastToken = scTk;
-				item = tokens.next();
 			}
-		} else {
-			yield tk;
 		}
-	}
-};
+
+		if (tk._.commandNameNotFoundYet) {
+			const last = iterable.behind(1);
+
+			if (!isOperator(last) && couldBeCommandName(tk)) {
+				tk._.maybeSimpleCommandName = true;
+			} else {
+				const next = iterable.ahead(1);
+				if (next && !couldEndSimpleCommand(next)) {
+					next._.commandNameNotFoundYet = true;
+				}
+			}
+			delete tk._.commandNameNotFoundYet;
+		}
+
+		return tk;
+	}),
+	lookahead
+);

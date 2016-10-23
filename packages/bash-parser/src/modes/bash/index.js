942eca86a816fd9333a1b231658218bc00ced671
Andrea Parodi <andrea@parro.it>
2016-10-23 18:46:27 +0200

    BASH: parameter substring

--- packages/bash-parser/src/modes/bash/index.js
+++ packages/bash-parser/src/modes/bash/index.js
@@ -2,6 +2,16 @@
 
 const bashAliasSubstitution = require('./rules/alias-substitution');
 
+const name = '[a-zA-Z_][a-zA-Z0-9_]*';
+const parameterOperators = {
+	[`^(${name}):(.*):(.*)$`]: {
+		op: 'substring',
+		parameter: m => m[1],
+		offset: m => parseInt(m[2], 10),
+		length: m => parseInt(m[3], 10)
+	}
+};
+
 module.exports = {
 	inherits: 'posix',
 	init: (posixMode, utils) => {
@@ -17,6 +27,21 @@ module.exports = {
 			posixMode.lexerPhases
 		);
 
-		return Object.assign({}, posixMode, {phaseCatalog, lexerPhases});
+		const bashOperators = Object.assign(
+			parameterOperators,
+			posixMode.enums.parameterOperators
+		);
+
+		const enums = Object.assign(
+			{},
+			posixMode.enums,
+			{parameterOperators: bashOperators}
+		);
+
+		return Object.assign(
+			{},
+			posixMode,
+			{phaseCatalog, lexerPhases, enums}
+		);
 	}
 };
--- packages/bash-parser/test/test-bash-mode.js
+++ packages/bash-parser/test/test-bash-mode.js
@@ -20,3 +20,20 @@ test('alias on reserved word', t => {
 		}]
 	});
 });
+
+test('parameter substitution with Substring Expansion', t => {
+	const result = bashParser('echo ${text:2:4}', {mode: 'bash'})
+		.commands[0].suffix[0].expansion[0];
+
+	utils.checkResults(t, result, {
+		loc: {
+			start: 0,
+			end: 10
+		},
+		parameter: 'text',
+		type: 'ParameterExpansion',
+		op: 'substring',
+		offset: 2,
+		length: 4
+	});
+});

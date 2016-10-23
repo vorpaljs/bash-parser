d50815d055a8c778e35c09a1949d41db0da4dae6
Andrea Parodi <andrea@parro.it>
2016-10-23 18:26:25 +0200

    fix arith subst test, restored bash test

---
+++ packages/bash-parser/test/test-bash-mode.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const test = require('ava');
+const bashParser = require('../src');
+const utils = require('./_utils');
+
+test('alias on reserved word', t => {
+	const result = bashParser('if world', {
+		mode: 'bash',
+		resolveAlias: name => {
+			return name === 'if' ? 'echo' : undefined;
+		}
+	});
+	utils.checkResults(t, result, {
+		type: 'Script',
+		commands: [{
+			type: 'Command',
+			name: {type: 'Word', text: 'echo'},
+			suffix: [{type: 'Word', text: 'world'}]
+		}]
+	});
+});

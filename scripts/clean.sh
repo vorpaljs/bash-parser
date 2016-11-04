adceb15c6a5d8bea08a0d34890ce01419d926519
Andrea Parodi <andrea@parro.it>
2016-11-04 09:33:48 +0100

    fix lean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,6 +2,8 @@ for package in `find ./packages/* -maxdepth 0`; do
  cd $package;
  rm -rf node_modules;
  rm -rf .nyc_output;
+ npm run clean --silent || echo clean task not found in $package
  cd ../..;
+
 done
 rm -rf docs/bundle.js;

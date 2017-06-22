6fa4ca52fbb92f9eb6b94e2e8c16768210b7af3d
Andrea Parodi <andrea@parro.it>
2017-06-22 21:56:14 +0200

    bash-parser is now the only package of the repo

--- packages/bash-parser/readme.md
+++
@@ -1,3 +0,0 @@
-# bash-parser
-
-> See [repo root readme](https://github.com/vorpaljs/bash-parser)
--- packages/shell-runtime/fixtures/env-spy.js
+++
@@ -1,2 +0,0 @@
-/* eslint-disable unicorn/no-process-exit */
-process.exit(Number(process.env.thisIsATest));
